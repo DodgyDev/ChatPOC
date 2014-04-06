cd84730f2466781f802ad17d8c79c501ec2df94b
lucaschenaw <lucaschen@air-watch.com>
2014-04-06 00:01:06 -0400

    Initial Commit

---
+++ MC Chat/MCCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MCCAppDelegate.h
+//  MC Chat
+//
+//  Created by Lucas Chen on 4/6/14.
+//  Copyright (c) 2014 GTLucas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MC ChatTests/MC_ChatTests.m
@@ -0,0 +1,34 @@
+//
+//  MC_ChatTests.m
+//  MC ChatTests
+//
+//  Created by Lucas Chen on 4/6/14.
+//  Copyright (c) 2014 GTLucas. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MC_ChatTests : XCTestCase
+
+@end
+
+@implementation MC_ChatTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
